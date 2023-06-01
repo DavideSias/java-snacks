3d2f01ffc698dffcd4961da707c2d714053483ca
DavideSias <davidesias77@gmail.com>
2023-06-01 15:21:19 +0200

    Refactor snack 3

--- src/org/lessons/java/Snack3.java
+++ src/org/lessons/java/Snack3.java
@@ -5,8 +5,10 @@ public class Snack3 {
         int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
         int total = 0;
 
-        for (int i = 1; i < numbers.length; i = i + 2) {
-            total += numbers[i];
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 != 0) {
+                total += numbers[i];
+            }
         }
 
         System.out.println(total);
